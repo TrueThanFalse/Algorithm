f2ec9d70bfada0517eb93c2d279e9263172093b3
TrueThanFalse <143565710+TrueThanFalse@users.noreply.github.com>
2023-10-08 15:52:15 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+        int size = message.length() * 2;
+        answer = size;
+        
+        return answer;
+    }
+}
