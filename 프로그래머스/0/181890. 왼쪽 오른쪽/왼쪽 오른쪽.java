cf1f2ad5c65c6cd114d29d11aca9e5f2212653b7
HongMinSik <143565710+TrueThanFalse@users.noreply.github.com>
2024-07-04 10:33:14 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+class Solution {
+    public String[] solution(String[] str_list) {
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < str_list.length; i++) {
+			if (str_list[i].equals("l")) {
+				for (int j = 0; j < i; j++) {
+					list.add(str_list[j]);
+				}
+				break;
+			} else if (str_list[i].equals("r")) {
+				for (int j = i + 1; j < str_list.length; j++) {
+					list.add(str_list[j]);
+				}
+				break;
+			}
+		}
+        
+        String[] answer = new String[list.size()];
+        return list.toArray(answer);
+    }
+}
